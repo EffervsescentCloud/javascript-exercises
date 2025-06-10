719098bc3552fec7397cc725ac3aed298a5d0c61
GEOFFREY Fisher Seufert <7003549@cpsd.us>
2025-06-10 12:59:37 -0400

    fixed the thing

--- 10_leapYears/leapYears.spec.js
+++ 10_leapYears/leapYears.spec.js
@@ -4,19 +4,19 @@ describe('leapYears', () => {
   test('works with non century years', () => {
     expect(leapYears(1996)).toBe(true);
   });
-  skip('works with non century years', () => {
+  test('works with non century years', () => {
     expect(leapYears(1997)).toBe(false);
   });
-  skip('works with ridiculously futuristic non century years', () => {
+  test('works with ridiculously futuristic non century years', () => {
     expect(leapYears(34992)).toBe(true);
   });
-  skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(1900)).toBe(false);
   });
-  skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(1600)).toBe(true);
   });
-  skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(700)).toBe(false);
   });
 });
