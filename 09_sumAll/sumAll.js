440f150cd6f7cd1948abba0973d23b7f75d52ec7
GEOFFREY Fisher Seufert <7003549@cpsd.us>
2025-06-12 12:26:18 -0400

    Update sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 function sumAll (num1, num2) {
-    if (!(Number.isInteger(num1) && Number.isInteger(num2))) {return 'ERROR'}
+    if (!(Number.isInteger(num1) && Number.isInteger(num2)) | num1 < 0 | num2 < 0) {return 'ERROR'}
     return Math.abs((1/2)*(num1**2 + num1) - (1/2)*(num2**2 + num2)) + (num2 > num1 ? num1 : num2)
 };
 
